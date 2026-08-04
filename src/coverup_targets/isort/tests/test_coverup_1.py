4d60e6a31a6849bc9b2071c0d06ffd189651a953
BinhNguyen <nguyenbinh39205@gmail.com>
2026-08-04 16:12:17 +0700

    update, fix lỗi chạy test thử coverup

---
+++ src/coverup_targets/isort/tests/test_coverup_1.py
@@ -0,0 +1,178 @@
+# file: sample_repo\isort\isort\core.py:32-490
+# asked: {"lines": [83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 117, 118, 119, 120, 122, 124, 129, 135, 138, 139, 140, 141, 142, 143, 144, 145, 146, 148, 150, 151, 152, 153, 154, 156, 157, 158, 168, 169, 173, 175, 177, 178, 179, 180, 181, 182, 183, 189, 190, 192, 194, 195, 196, 198, 199, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 220, 221, 222, 223, 228, 229, 231, 233, 234, 235, 237, 238, 239, 240, 241, 242, 243, 245, 246, 247, 248, 249, 250, 251, 253, 255, 256, 257, 258, 259, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 273, 274, 279, 280, 281, 282, 284, 285, 287, 290, 291, 292, 294, 302, 303, 304, 305, 306, 308, 309, 311, 312, 314, 315, 319, 321, 322, 335, 338, 342, 343, 345, 346, 347, 348, 349, 350, 351, 353, 356, 357, 359, 362, 366, 367, 368, 369, 370, 371, 376, 377, 378, 379, 380, 381, 382, 384, 385, 386, 387, 388, 389, 392, 393, 397, 398, 400, 401, 407, 420, 421, 435, 436, 437, 438, 449, 452, 453, 454, 457, 458, 464, 465, 469, 470, 471, 472, 474, 475, 478, 479, 480, 481, 483, 484, 487, 488], "branches": [[82, 83], [86, 87], [86, 124], [87, 88], [87, 91], [88, 89], [88, 90], [91, 94], [91, 122], [94, 95], [94, 96], [96, 97], [96, 120], [97, 98], [97, 101], [104, 105], [104, 107], [128, 129], [134, 135], [137, 138], [138, 139], [138, 141], [157, 158], [157, 364], [166, 168], [171, 186], [172, 173], [174, 175], [176, 177], [186, 192], [193, 198], [202, 203], [204, 205], [204, 206], [206, 207], [206, 225], [207, 208], [207, 209], [209, 210], [209, 214], [210, 211], [210, 223], [212, 213], [212, 223], [214, 215], [214, 221], [216, 217], [216, 220], [221, 222], [221, 223], [226, 364], [227, 228], [228, 229], [228, 364], [230, 231], [232, 233], [236, 237], [244, 245], [245, 246], [245, 273], [261, 262], [261, 264], [265, 266], [265, 267], [275, 279], [279, 280], [279, 284], [286, 287], [288, 294], [295, 362], [299, 302], [302, 303], [302, 308], [303, 299], [303, 304], [308, 299], [308, 309], [311, 312], [311, 314], [317, 321], [328, 338], [340, 345], [346, 347], [346, 353], [355, 356], [356, 357], [356, 359], [365, 366], [366, 367], [366, 369], [369, 370], [369, 371], [374, 384], [386, 387], [386, 388], [391, 392], [395, 464], [396, 397], [403, 406], [406, 407], [412, 419], [419, 420], [433, 451], [434, 435], [448, 449], [451, 452], [453, 454], [453, 456], [456, 457], [467, 126], [468, 469], [469, 470], [469, 477], [471, 472], [471, 474], [477, 478], [478, 126], [478, 479], [480, 481], [480, 483], [486, 487], [487, 488], [487, 490]]}
+# gained: {"lines": [83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 107, 108, 109, 111, 112, 113, 114, 115, 117, 118, 119, 120, 122, 124, 129, 138, 139, 140, 141, 142, 143, 144, 145, 146, 148, 150, 151, 152, 153, 154, 156, 157, 158, 168, 169, 173, 175, 177, 178, 179, 180, 182, 189, 190, 192, 194, 195, 196, 198, 199, 203, 204, 206, 207, 208, 209, 210, 211, 212, 214, 215, 216, 217, 218, 220, 221, 223, 228, 237, 238, 239, 240, 241, 242, 243, 279, 284, 285, 287, 290, 291, 292, 294, 302, 303, 304, 305, 306, 308, 309, 311, 312, 314, 315, 319, 321, 322, 335, 338, 345, 346, 347, 348, 349, 350, 351, 353, 362, 366, 367, 368, 407, 420, 421, 435, 436, 437, 438, 452, 453, 454, 457, 458, 464, 465, 478, 479, 480, 483, 484, 487, 488], "branches": [[82, 83], [86, 87], [86, 124], [87, 88], [87, 91], [88, 89], [88, 90], [91, 94], [91, 122], [94, 95], [94, 96], [96, 97], [96, 120], [97, 98], [104, 107], [128, 129], [137, 138], [138, 139], [157, 158], [166, 168], [172, 173], [174, 175], [176, 177], [186, 192], [193, 198], [202, 203], [204, 206], [206, 207], [206, 225], [207, 208], [207, 209], [209, 210], [209, 214], [210, 211], [210, 223], [212, 223], [214, 215], [214, 221], [216, 217], [216, 220], [221, 223], [226, 364], [227, 228], [228, 364], [236, 237], [275, 279], [279, 284], [286, 287], [288, 294], [295, 362], [299, 302], [302, 303], [302, 308], [303, 299], [303, 304], [308, 309], [311, 312], [311, 314], [317, 321], [328, 338], [340, 345], [346, 347], [346, 353], [365, 366], [366, 367], [395, 464], [403, 406], [406, 407], [412, 419], [419, 420], [434, 435], [451, 452], [453, 454], [453, 456], [456, 457], [467, 126], [477, 478], [478, 126], [478, 479], [480, 483], [486, 487], [487, 488], [487, 490]]}
+
+from io import StringIO
+import pytest
+from isort.core import process
+from isort.settings import DEFAULT_CONFIG, Config
+from isort.exceptions import FileSkipComment, ExistingSyntaxErrors
+
+def test_process_comprehensive_coverage():
+    # 1. float_to_top with isort: off, isort: on, isort: split, and add_imports
+    content = (
+        "# isort: off\n"
+        "import b\n"
+        "# isort: on\n"
+        "# isort: split\n"
+        "import sys\n"
+        "import os\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(float_to_top=True, add_imports=["import foo"])
+    res = process(input_stream, output_stream, config=config)
+    assert isinstance(res, bool)
+
+    # 2. force_adds = False at index 0 (empty stream)
+    input_stream = StringIO("")
+    output_stream = StringIO()
+    config = Config(force_adds=False)
+    assert process(input_stream, output_stream, config=config) is False
+
+    # 3. no line_separator, code_sorting and code_sorting_section (reexport)
+    content = "__all__ = ['a', 'b']\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(line_ending="", sort_reexports=True)
+    process(input_stream, output_stream, config=config)
+
+    # 4. FILE_SKIP_COMMENTS raise_on_skip = True and False
+    input_stream = StringIO("# isort: skip_file\n")
+    output_stream = StringIO()
+    with pytest.raises(FileSkipComment):
+        process(input_stream, output_stream, raise_on_skip=True)
+
+    input_stream = StringIO("# isort: skip_file\n")
+    output_stream = StringIO()
+    process(input_stream, output_stream, raise_on_skip=False)
+
+    # 5. dont-add-imports, dont-add-import
+    content = (
+        "# isort: dont-add-imports\n"
+        "# isort: dont-add-import: foo\n"
+        "import bar\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(add_imports=["foo", "bar"])
+    process(input_stream, output_stream, config=config)
+
+    # 6. top comments and section comments
+    content = (
+        "# Top comment\n"
+        "# section_comment\n"
+        "import os\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config()
+    object.__setattr__(config, "_section_comments", ("# section_comment",))
+    object.__setattr__(config, "_section_comments_end", ("# section_comment_end",))
+    process(input_stream, output_stream, config=config)
+
+    # 7. Quotes inside code, escaped chars, multiline quotes
+    content = (
+        'x = "hello \\" world"\n'
+        "y = '''multiline'''\n"
+        "z = \"abc\"\n"
+        "# comment\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    # 8. code_sorting trigger by comment and blank line ending code sorting
+    content = (
+        "# isort: sorted\n"
+        "b = 2\n"
+        "a = 1\n"
+        "\n"
+        "import os\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    # 9. section_comments and section_comments_end branch in imports vs not_imports
+    content = (
+        "# Third Party\n"
+        "import os\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config()
+    object.__setattr__(config, "_section_comments", ("# Third Party",))
+    process(input_stream, output_stream, config=config)
+
+    # 10. treat_all_comments_as_code / treat_comments_as_code
+    content = (
+        "# regular comment\n"
+        "import sys\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(treat_comments_as_code=["# regular comment"])
+    process(input_stream, output_stream, config=config)
+
+    # 11. Multiline import with backslash and unclosed parenthesis (SyntaxError)
+    content = (
+        "from os import \\\n"
+        "    path\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    content = "from os import (\n    path\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    with pytest.raises(ExistingSyntaxErrors):
+        process(input_stream, output_stream)
+
+    # 12. "from" without "import"
+    content = "from = 1\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    # 13. cimport identifiers
+    content = "import module.cimport\ncython.cimports.foo\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    # 14. cimport_statement != cimports or new_indent != indent branches
+    content = (
+        "import a\n"
+        "    cimport b\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    process(input_stream, output_stream)
+
+    # 15. lines_before_imports
+    content = "\n\nimport os\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(lines_before_imports=2)
+    process(input_stream, output_stream, config=config)
+
+    # 16. append_only, not_imports with output_stream writes and yield/raise loops
+    content = (
+        "yield \\\n"
+        "    value\n"
+        "raise \\\n"
+        "    ValueError\n"
+    )
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(append_only=True)
+    process(input_stream, output_stream, config=config)
+
+    # 17. only_modified verbose output
+    content = "import b\nimport a\n"
+    input_stream = StringIO(content)
+    output_stream = StringIO()
+    config = Config(only_modified=True, verbose=True)
+    process(input_stream, output_stream, config=config)
